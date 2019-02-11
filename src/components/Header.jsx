f4ab1a3190b6e0a868a1727fe48b1f6330284002
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-02-10 22:59:31 -0800

    new project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  var headerStyle = {
+    color: 'red',
+    backgroundColor: 'green'
+  }
+  
+  return(
+    <h1 style={headerStyle}>Help Queue</h1>
+  );
+}
+
+export default Header;
---
+++ src/components/TicketList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+
+function TicketList() {
+  return(
+    <Ticket
+      location="3A"
+      names= "Thato and Haley"
+      issue="Firebase problems" />
+  );
+}
+
+export default TicketList;
