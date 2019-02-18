5dd1605f0d0038206c7b3173f195dfbb3101fd9c
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-02-17 22:33:59 -0800

    updates with ticket creation

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,27 +1,50 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { v4 } from 'uuid'
 
-function NewTicketForm() {
+
+function NewTicketForm(props) {
+
+  let _names = null
+  let _location = null
+  let _issue = null
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTicketCreation({names: _names.value, location: _location.value, issue: _issue.value, id: v4()})
+    _names.value = '';
+    _location.value = '';
+    _issue.value = '';
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleNewTicketFormSubmission}>
         <input
           type='text'
           id='names'
-          placeholder='Pair Names' />
-          <br />
+          placeholder='Pair Names' 
+          ref={(input) => {_names = input}}/>
+        <br />
         <input
           type='text'
           id='location'
-          placeholder='Location' />
-          <br />
+          placeholder='Location' 
+          ref={(input) => {_location = input}}/>
+        <br />
         <textarea
           id='issue'
-          placeholder='Describe your issue.' />
-          <br />          
+          placeholder='Describe your issue.' 
+          ref={(input) => {_issue = input}}/>
+        <br />          
         <button type='submit'>Help!</button>
       </form>
     </div>
   )
+}
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes. func
 }
 
 export default NewTicketForm
