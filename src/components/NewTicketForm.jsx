c525c1f720b4ab7712dae11610b215499c1bddab
Guest User <Guest@epicodus-7.local>
2019-02-26 15:03:32 -0800

    updates with redux

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { v4 } from 'uuid'
-import Moment from 'react-moment'
+import React from 'react';
+import Moment from 'moment';
+import { connect } from 'react-redux';
+import { v4 } from 'uuid';
 
-
-function NewTicketForm(props) {
-
-  let _names = null
-  let _location = null
-  let _issue = null
+function NewTicketForm(props){
+  let _names = null;
+  let _location = null;
+  let _issue = null;
 
   function handleNewTicketFormSubmission(event) {
+    const { dispatch } = props;
     event.preventDefault();
-    props.onNewTicketCreation({names: _names.value, location: _location.value, issue: _issue.value, id: v4()})
+    const action = {
+      type: 'ADD_TICKET',
+      id: v4(),
+      names: _names.value,
+      location: _location.value,
+      issue: _issue.value,
+      timeOpen: new Moment()
+    };
+    dispatch(action);
     _names.value = '';
     _location.value = '';
     _issue.value = '';
@@ -24,28 +31,21 @@ function NewTicketForm(props) {
         <input
           type='text'
           id='names'
-          placeholder='Pair Names' 
-          ref={(input) => {_names = input}}/>
-        <br />
+          placeholder='Pair Names'
+          ref={(input) => {_names = input;}}/>
         <input
           type='text'
           id='location'
-          placeholder='Location' 
-          ref={(input) => {_location = input}}/>
-        <br />
+          placeholder='Location'
+          ref={(input) => {_location = input;}}/>
         <textarea
           id='issue'
-          placeholder='Describe your issue.' 
-          ref={(input) => {_issue = input}}/>
-        <br />          
+          placeholder='Describe your issue.'
+          ref={(textarea) => {_issue = textarea;}}/>
         <button type='submit'>Help!</button>
       </form>
     </div>
-  )
+  );
 }
 
-NewTicketForm.propTypes = {
-  onNewTicketCreation: PropTypes. func
-}
-
-export default NewTicketForm
+export default connect()(NewTicketForm);
